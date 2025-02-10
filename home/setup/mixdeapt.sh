6604eabe7d127d516a53e49922fef786b4066f5a
1@Computer Pakahm <1com.ipcam@gmail.com>
2025-02-10 21:27:58 +0700

    Add files via upload

---
+++ home/setup/mixdeapt.sh
@@ -0,0 +1,25 @@
+sudo apt update -y
+
+sudo apt install nala -y
+
+sudo nala update && sudo apt upgrade -y
+
+sudo nala clean
+
+xargs -a 1compkgs sudo apt install -y
+
+sudo nala update
+
+sudo nala clean
+
+xargs -a mixde sudo apt install
+
+sudo nala update
+
+sudo nala clean
+
+sudo apt install sddm firefox-esr -y
+
+sudo nala update
+
+sudo nala clean
