d1ff266b48ef9522dd0f91dc93eb61cd7242a8b6
1@Computer Pakahm <1com.ipcam@gmail.com>
2025-02-22 03:22:05 +0700

    Add files via upload

---
+++ home/setup/1com.sh
@@ -0,0 +1,21 @@
+sudo apt update -y
+
+sudo apt install nala -y
+
+sudo nala --install-completion bash
+
+sudo nala update && sudo apt upgrade -y
+
+sudo nala clean
+
+xargs -a 1compkgs sudo nala install
+
+sudo nala update
+
+sudo nala clean
+
+sudo nala install sddm -y
+
+sudo nala update
+
+sudo nala clean
