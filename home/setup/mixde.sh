0c912c8644fa3e037fc46d0e7b9954f0150debdf
1@Computer Pakahm <1com.ipcam@gmail.com>
2025-02-25 15:04:38 +0000

    Add files via upload

---
+++ home/setup/mixde.sh
@@ -0,0 +1,27 @@
+sudo apt update -y
+
+sudo apt install nala -y
+
+sudo nala --install-completion bash
+
+sudo nala update && sudo apt upgrade -y
+
+sudo nala clean
+
+xargs -a 1compkgs sudo nala install -y
+
+sudo nala update
+
+sudo nala clean
+
+xargs -a mixde sudo nala install
+
+sudo nala update
+
+sudo nala clean
+
+sudo nala install sddm -y
+
+sudo nala update
+
+sudo nala clean
