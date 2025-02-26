5000862723b44f82d3966396d253d6f1141de932
1@Computer Pakahm <1com.ipcam@gmail.com>
2025-02-26 15:26:27 +0000

    Update slaxbuild.sh

--- slaxbuild.sh
+++ slaxbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "deb https://mirror.sg.gs/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
-echo "deb https://mirror.twds.com.tw/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
+echo "deb https://mirror.sg.gs/debian/ bookworm main" | tee -a /etc/apt/sources.list
+echo "deb https://mirror.twds.com.tw/debian/ bookworm main" | tee -a /etc/apt/sources.list
 
 #passwd guest
 #usermod -l user guest
