4846572f13affe1188f9c7cd4654901d046455c3
1@Computer Pakahm <1com.ipcam@gmail.com>
2025-02-26 13:44:17 +0000

    Update slaxwifibuild.sh

--- slaxwifibuild.sh
+++ slaxwifibuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp nala-bookworm.list /etc/apt/sources.list.d
+echo "deb https://mirror.sg.gs/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
+echo "deb https://mirror.twds.com.tw/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
 
 #passwd guest
 #usermod -l user guest
