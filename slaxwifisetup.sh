63599a0107fcb01ddc8ca56607a71cf09d89182a
1@Computer Pakahm <1com.ipcam@gmail.com>
2025-03-04 20:08:04 +0000

    Add files via upload

---
+++ slaxwifisetup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Unblock wifi
+rfkill unblock all
+
+# Find wifi device (with manual selection if needed)
+wifi_device=$(ip a | grep -oE '^[0-9]+: [a-z]+[0-9]+:' | cut -d: -f2 | tr -d ' ')
+
+if [ -z "$wifi_device" ]; then
+  echo "Available Wi-Fi Interfaces:"
+  ip a | grep -oE '^[0-9]+: [a-z]+[0-9]+:' | cut -d: -f2 | tr -d ' ' | nl -w1 -s'. '
+  read -p "Enter the number of your Wi-Fi interface: " interface_num
+  wifi_device=$(ip a | grep -oE '^[0-9]+: [a-z]+[0-9]+:' | cut -d: -f2 | tr -d ' ' | sed -n "${interface_num}p")
+fi
+
+# Unblock wifi
+rfkill unblock wifi
+
+# Scan for SSID
+wpa_cli scan
+
+# Show wifi SSID
+wpa_cli scan_results
+
+# Prompt user for SSID and password
+read -p "Enter SSID: " ssid
+read -sp "Enter Password: " password
+echo
+
+# Setup wifi connection
+wpa_passphrase "$ssid" "$password" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null
+sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Start wpa_supplicant
+sudo killall -q wpa_supplicant
+sudo wpa_supplicant -B -i "$wifi_device" -c /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Check wifi status
+wpa_cli status
+
+# Run DHCP to get an IP
+dhclient "$wifi_device"
+
+# Test by ping Google
+ping -c 2 1.1.1.1
